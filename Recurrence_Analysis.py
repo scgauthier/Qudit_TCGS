de32be4dce2bc7fc47afec4419589725c358a1f9
scgauthier <scarlettgauthier@gmail.com>
2020-11-10 18:41:42 +0100

    Recurrence_Analysis: added routines for managing iteration of subprotocols P1 and P2. Added graphing for comparing input and output fidelities and success probabilities of purification stages

--- Recurrence_Analysis.py
+++ Recurrence_Analysis.py
@@ -8,6 +8,11 @@ from operator import add
 #dim: dimension of qudit
 #graph_type: GHZ, line
 #input_type: oneParam,
+#param: x for one param states, // related to input fidelity
+#iterations: Number of times to repeat purification protocol
+#first_subP: 'P1' or 'P2' // Choice determines which purification is performed first
+#alternation: True, Flase // If true, alternate between protocols P1 and P2
+#doplot: True, False // If true, plot fidelity over rounds, as well as success probability
 
 start_time=time.time()
 
@@ -17,14 +22,14 @@ def assign_nodes(num_nodes,graph_type):
         numB=num_nodes-1
     elif graph_type =='line':
         if (num_nodes % 2)==0:
-            numA=num_nodes/2
-            numB=num_nodes/2
+            numA=int(num_nodes/2)
+            numB=int(num_nodes/2)
         else:
-            numA=(num_nodes+1)/2
-            numB=(num_nodes-1)/2
+            numA=int((num_nodes+1)/2)
+            numB=int((num_nodes-1)/2)
     elif graph_type =='cluster':
-        numA=num_nodes/2
-        numB=num_nodes/2
+        numA=int(num_nodes/2)
+        numB=int(num_nodes/2)
     return numA,numB
 
 def get_input_coefficients(num_nodes,dim,graph_type,input_type,param):
@@ -81,12 +86,109 @@ def P1_update_coefficients(num_nodes,dim,graph_type,cmat_in):
                             coef_mat[x,control]+=((cmat_in[x,y])**2)/normK
 
 
-    return coef_mat
+    return normK,coef_mat
+
+def P2_update_coefficients(num_nodes,dim,graph_type,cmat_in):
+    dim_list=list(range(0,dim))
+
+    numA,numB=assign_nodes(num_nodes,graph_type)
+
+    normK=0
+    for x in range(0,dim**numB):
+        for y in range(0,dim**numA):
+            for z in range(0,y):
+                normK+=cmat_in[y,x]*cmat_in[z,x]+cmat_in[z,x]*cmat_in[y,x]
+            normK+=(cmat_in[y,x])**2
+
+    indices=list(product(dim_list,repeat=numA))
+    coef_mat=np.zeros((dim**numA,dim**numB))
+    for control in range(0,dim**numA):
+        indexControl=np.array(indices[control])
+        # good_entries=[]
+        for y in range(0,dim**numA):
+            indexA1=np.array(indices[y])
+
+            for z in range(0,y+1):
+                indexA2=np.array(indices[z])
+
+                for entry in range(0,numA):
+                    if ((indexA1+indexA2+(dim-1)*indexControl) % dim)[numA-1-entry]!=0:
+                        break
+                    elif entry==numA-1 and y!=z:
+                        for x in range(0,dim**numB):
+                            coef_mat[control,x]+=(cmat_in[y,x]*cmat_in[z,x]+cmat_in[z,x]*cmat_in[y,x])/normK
+                    elif entry==numA-1 and y==z:
+                        for x in range(0,dim**numB):
+                            coef_mat[control,x]+=((cmat_in[y,x])**2)/normK
 
 
-coef_mat=get_input_coefficients(3,3,'GHZ','oneParam',0.6)
-#print(coef_mat)
-print(P1_update_coefficients(3,3,'GHZ',coef_mat))
+    return normK,coef_mat
+
+def single_plot(fids,psuccs,pcum_list,subP):
+    xdat=range(0,np.size(fids))
+
+    fig,ax1=plt.subplots()
+
+    ax2=ax1.twinx()
+    ax1.plot(xdat,fids,'blue')
+    ax2.plot(xdat,pcum_list,'co',label='Cumulative Probability')
+    ax2.plot(xdat,psuccs,'ro',label='Stage probability')
+    ax1.set_xlabel('Purifications applied (beginning with {})'.format(subP))
+    ax1.set_ylabel('Fidelity',color='blue')
+    ax2.set_ylabel('Probaility of success')
+    ax2.legend()
+    plt.show()
+
+def run_purification(num_nodes,dim,graph_type,input_type,param,iters,subP,alternation,doplot):
+    csubP=subP
+    fids=[]
+    psucc_inst=[None]
+    pcum_list=[None]
+    pcum=1
+    #prepare input P1_update_coefficients
+    coef_mat=get_input_coefficients(num_nodes,dim,graph_type,input_type,param)
+    fids.append(coef_mat[0,0])
+
+    for x in range(0,iters):
+        if csubP == 'P1':
+            normK,coef_mat=P1_update_coefficients(num_nodes,dim,graph_type,coef_mat)
+            if alternation==True:
+                csubP='P2'
+
+        elif csubP == 'P2':
+            normK,coef_mat=P2_update_coefficients(num_nodes,dim,graph_type,coef_mat)
+            if alternation==True:
+                csubP='P1'
+
+        fids.append(coef_mat[0,0])
+        psucc_inst.append(normK)
+        pcum*=normK
+        pcum_list.append(pcum)
+
+    if doplot == True:
+        single_plot(fids,psucc_inst,pcum_list,subP)
+
+    return fids
+
+def manyD_plot(num_nodes,dimlist,graph_type,input_type,param,iters,subP,alternation):
+
+    xdat=range(iters+1)
+    for dim in dimlist:
+        ydat=run_purification(num_nodes,dim,graph_type,input_type,param,iters,subP,alternation,False)
+        plt.plot(xdat,ydat,'o',label='Dimension = {}'.format(dim))
+    plt.legend()
+    plt.locator_params(axis='x',nbins=iters+1)
+    plt.xlabel('Number of purifications (beginning with {})'.format(subP))
+    plt.ylabel('Fidelity')
+    plt.show()
+
+
+#run_purification(5,3,'line','oneParam',0.6,6,'P2',True,False)
+# do_plotting(fids,psuccs,pcum_list,'P1')
+#manyD_plot(4,[2,3,5,7,9],'line','oneParam',0.6,4,'P1',True)
+
+coef_mat=get_input_coefficients(3,3,'GHZ','oneParam',0.04)
+print(P2_update_coefficients(3,3,'GHZ',coef_mat))
 
 
 print("--- %s seconds ---" % (time.time()-start_time))
