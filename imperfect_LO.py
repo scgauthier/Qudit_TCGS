3a8c29f883e3e476009c9aba51c728ef3fb8696d
scgauthier <scarlettgauthier@gmail.com>
2021-01-21 18:46:38 +0100

    More tweaks

--- imperfect_LO.py
+++ imperfect_LO.py
@@ -534,29 +534,30 @@ def get_purification_range(dim,num_nodes,graph_type,state_param_list,gate_er_par
                         break
         max_fids[x]=max(FO[(iters-1)*repeats : iters*repeats])
 
-        plt.figure()
-        # plt.plot(state_param_list,FI[0:repeats],label='Initial Fidelity')
-        for z in range(iters):
-            if (z%2)!=0 or z==0 or z==(iters-1):
-                plt.plot(state_param_list,FO[z*repeats:((z+1)*repeats)],label='F out iteration {}'.format(z))
-        plt.legend()
-        plt.xlabel('Depolarization channel parameter q',fontsize=18)
-        plt.ylabel('Fidelity to perfect graph state', fontsize=18)
-        plt.title('Gate Error = {}, dim={}, N={}'.format(gate_er,dim,num_nodes))
-        figname='../Figures/GateEr_Fids_{}_{}_{}_{}.jpg'.format(dim,num_nodes,gate_er,subP)
-        plt.savefig(figname,dpi=300)
-
-        plt.figure()
-        for z in range(iters):
-            if (z%2)!=0 or z==0 or z==(iters-1):
-                plt.plot(state_param_list,slopes[(z*repeats):((z+1)*repeats)],label='Iteration {}'.format(z))
-        plt.legend()
-        plt.xlabel('Depolarization channel parameter q', fontsize=18)
-        plt.ylabel('Instantaneous rate of change of fidelity')
-        plt.title('Gate Error = {}, dim={}, N={}'.format(gate_er,dim,num_nodes))
-        figname='../Figures/GateEr_Slopes_{}_{}_{}_{}.jpg'.format(dim,num_nodes,gate_er,subP)
-        plt.savefig(figname,dpi=300)
-        plt.close()
+        mypool.close()
+        # plt.figure()
+        # # plt.plot(state_param_list,FI[0:repeats],label='Initial Fidelity')
+        # for z in range(iters):
+        #     if (z%2)!=0 or z==0 or z==(iters-1):
+        #         plt.plot(state_param_list,FO[z*repeats:((z+1)*repeats)],label='F out iteration {}'.format(z))
+        # plt.legend()
+        # plt.xlabel('Depolarization channel parameter q',fontsize=18)
+        # plt.ylabel('Fidelity to perfect graph state', fontsize=18)
+        # plt.title('Gate Error = {}, dim={}, N={}'.format(gate_er,dim,num_nodes))
+        # figname='../Figures/GateEr_Fids_{}_{}_{}_{}.jpg'.format(dim,num_nodes,gate_er,subP)
+        # plt.savefig(figname,dpi=300)
+        #
+        # plt.figure()
+        # for z in range(iters):
+        #     if (z%2)!=0 or z==0 or z==(iters-1):
+        #         plt.plot(state_param_list,slopes[(z*repeats):((z+1)*repeats)],label='Iteration {}'.format(z))
+        # plt.legend()
+        # plt.xlabel('Depolarization channel parameter q', fontsize=18)
+        # plt.ylabel('Instantaneous rate of change of fidelity')
+        # plt.title('Gate Error = {}, dim={}, N={}'.format(gate_er,dim,num_nodes))
+        # figname='../Figures/GateEr_Slopes_{}_{}_{}_{}.jpg'.format(dim,num_nodes,gate_er,subP)
+        # plt.savefig(figname,dpi=300)
+        # plt.close()
 
 
     #Keep record of min fids, input fidelities
